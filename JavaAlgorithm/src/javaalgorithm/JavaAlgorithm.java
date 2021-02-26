a9d6293250018e04f78c37dae1f8b526ab031349
yan zhang <zhang78@shedisancollege.com>
2021-02-25 19:54:39 -0500

    testing commit

---
+++ JavaAlgorithm/src/javaalgorithm/JavaAlgorithm.java
@@ -0,0 +1,19 @@
+
+
+package javaalgorithm;
+
+/**
+ *This class +++Insert Description Here+++
+ *testing
+ * @author Yan Zhang
+ */
+public class JavaAlgorithm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+
+}
